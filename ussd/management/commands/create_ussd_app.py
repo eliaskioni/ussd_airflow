06aa3f9298dd1716f563532f694caf49f2fc201b
Elias Kioni <eliaskioni94@gmail.com>
2017-01-25 11:15:35 +0300

    get all space separated user input from terminal only one app name is required by django, Raise an error if more than one app name is provided

--- ussd/management/commands/create_ussd_app.py
+++ ussd/management/commands/create_ussd_app.py
@@ -2,6 +2,7 @@ from django.core.management import BaseCommand
 from django.core.management import CommandError
 from django.core.management import call_command
 from ussd_airflow.settings import BASE_DIR
+
 path = BASE_DIR + '/ussd_airflow_app_tpl'
 
 
@@ -13,7 +14,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            if len(options.get('ussd_app_name')) > 1:
+            if len(options.get('ussd_app_name')[0].split()) > 1:
                 raise CommandError
             app_name = options.get('ussd_app_name')[0]
             call_command('startapp', app_name, template=path)
