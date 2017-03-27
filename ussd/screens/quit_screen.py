910bec3ec99390f74b3d9923610536c9bdc8ef3d
eliaskioni <eliaskioni94@gmail.com>
2017-03-28 01:27:15 +0300

    fire off a signal when quit screen is handled

--- ussd/screens/quit_screen.py
+++ ussd/screens/quit_screen.py
@@ -1,6 +1,8 @@
 from ussd.core import UssdHandlerAbstract, UssdResponse
 from ussd.screens.serializers import UssdContentBaseSerializer
 from ussd import defaults as ussd_airflow_variables
+from ussd.signals import session_ended
+
 
 class QuitScreen(UssdHandlerAbstract):
     """
@@ -21,8 +23,10 @@ class QuitScreen(UssdHandlerAbstract):
 
         if self.initial_screen.get('ussd_report_session'):
             # schedule a task to report session
-            self.fire_ussd_report_session_task(self.initial_screen,
-                                               self.ussd_request.session_id,
-                                               support_countdown=False)
+            ended_kwargs = {
+                "session_key": self.ussd_request.session_id}
+            session_ended.send(sender=self.__class__,
+                               **ended_kwargs
+                               )
 
         return UssdResponse(self.get_text(), status=False)
